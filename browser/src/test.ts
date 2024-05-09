cc3d094709cfe496e3cc3e95a8d73db8b0752d23
Thaeriem <kauly.cs@gmail.com>
2024-05-08 19:04:38 -0700

    start TS

--- browser/src/test.ts
+++ browser/src/test.ts
@@ -3,4 +3,4 @@ const ansi_up = new AnsiUp();
 
 const txt  = "\n\n\x1B[1;33;40m 33;40  \x1B[1;33;41m 33;41  \x1B[1;33;42m 33;42  \x1B[1;33;43m 33;43  \x1B[1;33;44m 33;44  \x1B[1;33;45m 33;45  \x1B[1;33;46m 33;46  \x1B[1m\x1B[0\n\n\x1B[1;33;42m >> Tests OK\n\n"
 
-let html = ansi_up.ansi_to_html(txt); 
+let html = ansi_up.ansi_to_html(txt);
