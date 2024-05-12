9d9cd4d1d46cea91744df3d65073d00e4a54e463
Thaeriem <kauly.cs@gmail.com>
2024-05-11 18:15:29 -0700

    finished auto reload functionality

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,21 +1,33 @@
 import * as vscode from "vscode";
-import {
-    default as AnsiUp
-} from 'ansi_up';
+import { default as AnsiUp } from 'ansi_up';
+import * as fs from 'fs';
+
 
 export function activate(context: vscode.ExtensionContext) {
     console.log("Extension activated");
-
+    const extensionPath = vscode.extensions.getExtension('Thaeriem.art2ascii')?.extensionPath;
+    const config = vscode.workspace.getConfiguration();
+    config.update("art2ascii.gifUri", extensionPath + "/output.data", 
+    vscode.ConfigurationTarget.Global);
     const provider = new CustomSidebarViewProvider(context.extensionUri);
+
     context.subscriptions.push(
         vscode.window.registerWebviewViewProvider(
             CustomSidebarViewProvider.viewType,
             provider
         )
     );
+
+    fs.watch(extensionPath + "/output.data", (eventType, filename) => {
+        if (eventType === 'change') {
+          console.log(`File ${filename} has been updated`);
+          provider.stopInterval().then(() => {provider.startInterval()});
+        }
+      });
+
     let uploadArt = vscode.commands.registerCommand(
         "art2ascii.upload-art",
-        () => {
+        async () => {
             const options: vscode.OpenDialogOptions = {
                 canSelectFiles: true,
                 canSelectMany: false,
@@ -34,24 +46,27 @@ export function activate(context: vscode.ExtensionContext) {
                         vscode.workspace.getConfiguration()
                         .update("art2ascii.gifPath", selectedGifPath, 
                         vscode.ConfigurationTarget.Global);
-                        vscode.window.showInformationMessage(`Selected file: ${selectedGifPath}`);
                     }
                 }
             });
+            try {
+                await vscode.commands.executeCommand('art2ascii.terminal');
+            } catch (error) {
+                vscode.window.showErrorMessage("Issue with rendering image.");
+            }
+
     });
 
     context.subscriptions.push(uploadArt);
 
     let terminal = vscode.commands.registerCommand(
         "art2ascii.terminal",
-        () => {
-            const extensionPath = vscode.extensions.getExtension('Thaeriem.art2ascii')?.extensionPath;
+        async () => {
             if (!extensionPath) {
                 vscode.window.showErrorMessage('Failed to retrieve extension directory path.');
                 return;
             }
 
-            const config = vscode.workspace.getConfiguration();
             var gifPath: string | undefined = config.get<string>('art2ascii.gifPath');
             if (gifPath == undefined) 
                 gifPath = "";
@@ -61,25 +76,17 @@ export function activate(context: vscode.ExtensionContext) {
             }
             const terminal = vscode.window.createTerminal(options);
 
-            const predeterminedCommand = 'art2ascii -f ' + gifPath + ' -w 10 -e -o ' + extensionPath; 
+            const predeterminedCommand = 'art2ascii -f ' + gifPath + ' -w 35 -e -o ' + extensionPath; 
             terminal.sendText(predeterminedCommand);
-            terminal.show();
-            vscode.workspace.getConfiguration()
-            .update("art2ascii.gifUri", extensionPath + "/output.data", 
-            vscode.ConfigurationTarget.Global);
-
-        }
-    )
-
-
+    });
     
     context.subscriptions.push(terminal);
 }
 
-
 class CustomSidebarViewProvider implements vscode.WebviewViewProvider {
     public static readonly viewType = "art2ascii.artView";
     private _view?: vscode.WebviewView;
+    private _intervalId?: NodeJS.Timeout;
 
     constructor(private readonly _extensionUri: vscode.Uri) {}
 
@@ -93,27 +100,49 @@ class CustomSidebarViewProvider implements vscode.WebviewViewProvider {
             enableScripts: true,
             localResourceRoots: [this._extensionUri],
         };
-
-        this.readFileAsDataUri("output.data").then((output:string) => {
-
-            const ansi_up = new AnsiUp();
-            const frames = output.split('@').map(frame => {
-                // Convert each frame to HTML
-                const html = ansi_up.ansi_to_html(frame);
-                // Wrap HTML content with dark grey background style
-                const darkGreyBackgroundStyle = `<style>body { background-color: #333; }</style>`;
-                return `<pre>${darkGreyBackgroundStyle}${html}</pre>`;
-            });
-            let currentIndex = 0;
-            setInterval(() => {
-                // Display current frame
-                webviewView.webview.html = frames[currentIndex];
-                // Move to next frame
-                currentIndex = (currentIndex + 1) % frames.length;
-            }, 1000); // Adjust interval as needed
-            
-        });
+        this.updateWebviewContent();
     }
+
+    public async stopInterval() {
+        if (this._intervalId) {
+            clearInterval(this._intervalId);
+            this._intervalId = undefined;
+        }
+    }
+
+    public startInterval() {
+        this.updateWebviewContent();
+    }
+
+    private async updateWebviewContent(): Promise<void> {
+
+        const frames = await this.getFrames("output.data");
+        let currentIndex = 0;
+        const interval = setInterval(() => {
+
+            this._view!.webview.html = frames[currentIndex];
+            // Move to next frame
+            currentIndex = (currentIndex + 1) % frames.length;
+        }, 83);
+        this._intervalId = interval;
+
+    }
+
+    private async getFrames(filename: string): Promise<string[]> {
+        const output = await this.readFileAsDataUri(filename);
+        const ansi_up = new AnsiUp();
+        let frames = output.split('@FRAME@').map(frame => {
+            // Convert each frame to HTML
+            const html = ansi_up.ansi_to_html(frame);
+            // Wrap HTML content with dark grey background style
+            const darkGreyBackgroundStyle = `<style>body { background-color: #333; }</style>`;
+            return `<pre>${darkGreyBackgroundStyle}${html}</pre>`;
+        });
+        frames.shift();
+        frames.pop();
+        return frames;
+    }
+
     private async readFileAsDataUri(filename: string): Promise<string> {
         try {
             const fileUri = vscode.Uri.joinPath(this._extensionUri, filename);
@@ -122,28 +151,8 @@ class CustomSidebarViewProvider implements vscode.WebviewViewProvider {
             return contentString;
         } catch (error) {
             console.error(`Error reading file ${filename}: ${error}`);
-            return ''; // Return an empty string in case of error
+            return '';
         }
-    }
-
-    private getHtmlContent(webview: vscode.Webview, dataUri: string): string {
-        const stylesheetUri = webview.asWebviewUri(
-            vscode.Uri.joinPath(this._extensionUri, "assets", "main.css")
-        );
-
-        return `
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <link rel="stylesheet" href="${stylesheetUri}" />
-            </head>
-            <body>
-                <section>
-                    <pre style="white-space: pre-wrap;">${dataUri}</pre>
-                </section>
-            </body>
-        </html>
-        `;
     }
 }
 
