dc8304c91ec05a826b419314b9510f99cacb6b32
Thaeriem <kauly.cs@gmail.com>
2024-05-26 14:45:41 +0300

    fixed file system bug (by removing it completely)

--- vsce-extension/src/extension.ts
+++ vsce-extension/src/extension.ts
@@ -15,6 +15,11 @@ export function activate(context: vscode.ExtensionContext) {
             provider
         )
     );
+    let selectedGifPath = "";
+    var gifPath: string | undefined = config.get<string>('art2ascii.gifPath');
+    if (gifPath == undefined) 
+        gifPath = "";   
+    provider.renderFrames(gifPath);
 
     let uploadArt = vscode.commands.registerCommand(
         "art2ascii.upload-art",
@@ -28,13 +33,13 @@ export function activate(context: vscode.ExtensionContext) {
                 }
             };
     
-            vscode.window.showOpenDialog(options).then(fileUri => {
+            vscode.window.showOpenDialog(options).then(async (fileUri) => {
                 if (fileUri == undefined) 
                     vscode.window.showInformationMessage("No file selected");
                 else {
                     if (fileUri && fileUri.length > 0) {
-                        const selectedGifPath = fileUri[0].fsPath;
-                        config.update("art2ascii.gifPath", selectedGifPath, 
+                        selectedGifPath = fileUri[0].fsPath;
+                        await config.update("art2ascii.gifPath", selectedGifPath, 
                         vscode.ConfigurationTarget.Global);
                         vscode.commands.executeCommand('art2ascii.render');
                     }
@@ -50,49 +55,19 @@ export function activate(context: vscode.ExtensionContext) {
             if (!extensionPath) {
                 vscode.window.showErrorMessage('Failed to retrieve extension directory path.');
                 return;
-            }
-
-            var gifPath: string | undefined = config.get<string>('art2ascii.gifPath');
-            if (gifPath == undefined) 
-                gifPath = "";
-            
-            const args: Args = {
-                filename: gifPath,
-                width: 35
-            }
-            const output = await art2ascii(args);
-            writeOutput(extensionPath + "/output.data", output);
-            // vscode.commands.executeCommand('workbench.action.reloadWindow');
+            } 
+            provider.renderFrames(selectedGifPath);
         });
     
     context.subscriptions.push(render);
 
 }
 
-async function writeOutput(filepath: string, data: string) {
-    const retries = 5;
-    const delay = 1000;
-    function attemptWrite(attempt: number) {
-        fs.writeFile(filepath, data, (err) => {
-          if (err) {
-            if ((err.code === 'EBUSY' || err.code === 'EACCES') && attempt < retries) {
-              console.error(`File is locked or access is denied. Retrying... (Attempt ${attempt + 1})`);
-              setTimeout(() => attemptWrite(attempt + 1), delay);
-            } else {
-              console.error('Error writing file:', err);
-            }
-          } else {
-            console.log('File written successfully');
-          }
-        });
-      }
-
-      attemptWrite(0);
-}
-
 class CustomSidebarViewProvider implements vscode.WebviewViewProvider {
     public static readonly viewType = "art2ascii.artView";
     private _view?: vscode.WebviewView;
+    private _frames: string[] = [`<pre></pre>`];
+    private _framesChanged: boolean = false;
 
     constructor(private readonly _extensionUri: vscode.Uri) {}
 
@@ -111,39 +86,44 @@ class CustomSidebarViewProvider implements vscode.WebviewViewProvider {
     }
 
     private async updateWebviewContent(): Promise<void> {
-        const watcher = 
-            vscode.workspace.createFileSystemWatcher(
-                new vscode.RelativePattern(this._extensionUri, 'output.data'));
-        watcher.onDidChange(uri => { console.log("FILE UPDATED"); });
-        let frames = await this.getFrames("output.data");
+        let local_frames = this._frames;
 
         const animate = (): NodeJS.Timeout => {
             let currentIndex = 0;
             const interval = setInterval(() => {
-                this._view!.webview.html = frames[currentIndex];
-                // Move to next frame
-                currentIndex = (currentIndex + 1) % frames.length;
+                this._view!.webview.html = local_frames[currentIndex];
+                currentIndex = (currentIndex + 1) % local_frames.length;
             }, 83);
             return interval;
         }
         let interval = animate();
         setInterval(async () => {
-            watcher.onDidChange(async (uri) => {
-                frames = await this.getFrames("output.data");
+            if (this._framesChanged) {
+                local_frames = this._frames;
                 clearInterval(interval);
                 this.clearHTML();
                 interval = animate();
-            });
+                this._framesChanged = false;
+            };
         }, 1000);
     }
     private async clearHTML() {
         this._view!.webview.html = ``;
     }
 
-    private async getFrames(filename: string): Promise<string[]> {
-        const output = await this.readFileAsDataUri(filename);
+    public async renderFrames(filename: string) {
+        const args: Args = {
+            filename: filename,
+            width: 35
+        }
+        const output = await art2ascii(args);
+        this._frames = this.getFrames(output);
+        this._framesChanged = true;
+    }
+
+    private getFrames(data: string): string[] {
         const ansi_up = new AnsiUp();
-        let frames = output.split('@FRAME@').map(frame => {
+        let frames = data.split('@FRAME@').map(frame => {
             // Convert each frame to HTML
             const html = ansi_up.ansi_to_html(frame);
             return `<pre>${html}</pre>`;
@@ -151,18 +131,6 @@ class CustomSidebarViewProvider implements vscode.WebviewViewProvider {
         frames.shift();
         frames.pop();
         return frames;
-    }
-
-    private async readFileAsDataUri(filename: string): Promise<string> {
-        try {
-            const fileUri = vscode.Uri.joinPath(this._extensionUri, filename);
-            const fileContent = await vscode.workspace.fs.readFile(fileUri);
-            const contentString = Buffer.from(fileContent).toString('utf-8');
-            return contentString;
-        } catch (error) {
-            console.error(`Error reading file ${filename}: ${error}`);
-            return '';
-        }
     }
 }
 
