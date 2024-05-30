fb5f72a7ae145db760de9da5ec011fae6dd1c9c0
Thaeriem <kauly.cs@gmail.com>
2024-05-30 21:09:18 +0300

    start gradient feature

---
+++ vsce-extension/src/art2ascii/a2a_color.ts
@@ -0,0 +1,75 @@
+export type Pixel = [number, number, number];
+export type ColorMapping = { [key: string]: string };
+// courtesy of https://stackoverflow.com/questions/3080421/javascript-color-gradient
+function getGradientColor (startColor: string, endColor: string, percent: number): string {
+    // strip the leading # if it's there
+    startColor = startColor.replace(/^\s*#|\s*$/g, '');
+    endColor = endColor.replace(/^\s*#|\s*$/g, '');
+  
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if (startColor.length === 3) {
+      startColor = startColor.replace(/(.)/g, '$1$1');
+    }
+  
+    if (endColor.length === 3) {
+      endColor = endColor.replace(/(.)/g, '$1$1');
+    }
+  
+    // get colors
+    const startRed = parseInt(startColor.substring(0, 2), 16),
+      startGreen = parseInt(startColor.substring(2, 4), 16),
+      startBlue = parseInt(startColor.substring(4, 6), 16);
+  
+    const endRed = parseInt(endColor.substring(0, 2), 16),
+      endGreen = parseInt(endColor.substring(2, 4), 16),
+      endBlue = parseInt(endColor.substring(4, 6), 16);
+  
+    // calculate new color
+    let diffRed = endRed - startRed;
+    let diffGreen = endGreen - startGreen;
+    let diffBlue = endBlue - startBlue;
+  
+    diffRed = ((diffRed * percent) + startRed);
+    diffGreen = ((diffGreen * percent) + startGreen);
+    diffBlue = ((diffBlue * percent) + startBlue);
+  
+    let diffRedStr = diffRed.toString(16).split('.')[0];
+    let diffGreenStr = diffGreen.toString(16).split('.')[0];
+    let diffBlueStr = diffBlue.toString(16).split('.')[0];
+  
+    // ensure 2 digits by color
+    if (diffRedStr.length === 1) diffRedStr = '0' + diffRedStr;
+    if (diffGreenStr.length === 1) diffGreenStr = '0' + diffGreenStr;
+    if (diffBlueStr.length === 1) diffBlueStr = '0' + diffBlueStr;
+  
+    return '#' + diffRedStr + diffGreenStr + diffBlueStr;
+}
+  
+export function GradientDriver (startColor: string, endColor: string): string[] {
+  let ret = [];
+  for (let i = 0.1; i < 1.0; i+= 0.1) {
+    ret.push(getGradientColor(startColor, endColor, i));
+  }
+  return ret;
+}
+
+export function parseColorFile(colorText: string): ColorMapping {
+  const colorMapping: ColorMapping = {};
+  const regex = /\((\d+), (\d+), (\d+)\): '(\d+)'/g;
+
+  let match: RegExpExecArray | null;
+  while ((match = regex.exec(colorText)) !== null) {
+    const rgb: Pixel = [
+      parseInt(match[1]),
+      parseInt(match[2]),
+      parseInt(match[3]),
+    ];
+    const code: string = match[4];
+    colorMapping[rgb.toString()] = code;
+  }
+  return colorMapping;
+}
+
+export function asciiColor(colorCode: number, text: string): string {
+  return `\x1b[38;5;${colorCode}m${text}\x1b[0m`;
+}
